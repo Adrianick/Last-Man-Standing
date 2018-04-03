4a1f7a910911c37f9d61e053b1873b4f6da02aac
Adrianick <adrian.danut.nicolae@gmail.com>
2018-04-03 16:49:03 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/SurvivalGamePooProiect2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SurvivalGamePooProiect2.dir/main.cpp.obj"
+  "SurvivalGamePooProiect2.pdb"
+  "SurvivalGamePooProiect2.exe"
+  "SurvivalGamePooProiect2.exe.manifest"
+  "libSurvivalGamePooProiect2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SurvivalGamePooProiect2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/SurvivalGamePooProiect2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for SurvivalGamePooProiect2.
+# This may be replaced when dependencies are built.
